709c574b61113df1105ab074299448b1153612f7
MuhammadRamzan <sp23-bse-027@cuiatk.edu.pk>
2026-02-06 22:43:56 +0500

    console based calculator

---
+++ Dart_Task1/tasks/t1.dart
@@ -0,0 +1,14 @@
+void main(){
+  String name ="Muhammad Ramzan";
+  int age = 23;
+  var skill ="Fultter";
+  print("Name: $name \nAge: $age \nSkill: $skill");
+
+  num number = 232;
+   if(number%2==0){
+    print("$number is even number");
+  }
+  else{
+    print("$number is odd number");
+  }
+}
