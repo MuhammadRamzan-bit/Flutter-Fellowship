8e16f017f146aee812a42976793ab49119187913
MuhammadRamzan <sp23-bse-027@cuiatk.edu.pk>
2026-02-10 19:47:35 +0500

    widgets
    text
    container
    column

--- demo_app/lib/Home_page.dart
+++ demo_app/lib/Home_page.dart
@@ -9,14 +9,65 @@ class HomePage extends StatelessWidget {
     final String name = "Ramzan";
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.purpleAccent,
         title: Text("myApp"),
       ),
       body: Center(
-        child: Text("welcome to $days days of flutter by $name",
-        style: TextStyle(fontSize: 20)),
-
+        child: Column(
+          children: [
+            Container(
+              width: 300,
+              height: 130,
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.all(8),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 212, 130, 226),
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color.fromARGB(255, 71, 24, 60),
+                    blurRadius: 10,
+                    spreadRadius: 2,
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "welcome to $days days of flutter by $name",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                      fontWeight: FontWeight.bold,
+                      backgroundColor: const Color.fromARGB(255, 142, 223, 35),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "here we add childern list",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                      fontWeight: FontWeight.bold,
+                      backgroundColor: const Color.fromARGB(255, 142, 223, 35),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-      drawer: Drawer(),
+
+      drawer: Drawer(
+        backgroundColor: Colors.cyan,
+        child: Text("login", style: TextStyle(fontSize: 20)),
+      ),
     );
   }
 }
--- demo_app/lib/main.dart
+++ demo_app/lib/main.dart
@@ -1,4 +1,4 @@
-import "package:demo_app/Home_page.dart";
+import "package:demo_app/home_page.dart";
 import "package:flutter/material.dart";
 
 void main(){
